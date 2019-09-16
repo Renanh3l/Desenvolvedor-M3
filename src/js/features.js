4997e561250621124061cc264dec36d86a659def
Renan Henrique <nan.pp@hotmail.com>
2019-09-16 16:50:51 -0300

    Melhorias na exibição de produtos

--- src/js/features.js
+++ src/js/features.js
@@ -1,72 +1,83 @@
 
 // Abrir ou fechar a janela de Filtro (versão mobile)
 function toggleMobileFilterModal() {
-    let div = document.getElementById("mobile-filter-modal");
+    if (window.matchMedia("(max-width: 700px)").matches) {
+        let div = document.getElementById("mobile-filter-modal");
 
-    div.style.display == "block" ? div.style.display = "none" : div.style.display = "block";
+        div.style.display == "block" ? div.style.display = "none" : div.style.display = "block";
+    }
+    
 }
 
 // Abrir ou fechar a janela de Ordenar (versão mobile)
 function toggleMobileOrderModal() {
-    let div = document.getElementById("mobile-order-modal");
+    if (window.matchMedia("(max-width: 700px)").matches) {
+        let div = document.getElementById("mobile-order-modal");
 
-    div.style.display == "block" ? div.style.display = "none" : div.style.display = "block";
+        div.style.display == "block" ? div.style.display = "none" : div.style.display = "block";
+    }
 }
 
 // Abrir ou fechar a filtro de Cores (versão mobile)
 function openColorFilter() {
-    let div = document.getElementById("mobile-color-filter");
-    let openIcon = document.getElementById("fa-color-open");
-    let closeIcon = document.getElementById('fa-color-close');
+    if (window.matchMedia("(max-width: 700px)").matches) {
+        let div = document.getElementById("mobile-color-filter");
+        let openIcon = document.getElementById("fa-color-open");
+        let closeIcon = document.getElementById('fa-color-close');
 
-    if (div.style.display == "flex") {
-        // Se estiver aberto
-        div.style.display = "none"; // Fechar
-        openIcon.style.display = "block";
-        closeIcon.style.display = "none";
-    } else {
-        // Se estiver fechado
-        div.style.display = "flex"; // Abrir
-        closeIcon.style.display = "block";
-        openIcon.style.display = "none";
+        if (div.style.display == "flex") {
+            // Se estiver aberto
+            div.style.display = "none"; // Fechar
+            openIcon.style.display = "block";
+            closeIcon.style.display = "none";
+        } else {
+            // Se estiver fechado
+            div.style.display = "flex"; // Abrir
+            closeIcon.style.display = "block";
+            openIcon.style.display = "none";
+        }
     }
 }
 
 // Abrir ou fechar a filtro de Tamanhos (versão mobile)
 function openSizeFilter() {
-    let div = document.getElementById("mobile-size-filter");
-    let openIcon = document.getElementById("fa-size-open");
-    let closeIcon = document.getElementById('fa-size-close');
+    if (window.matchMedia("(max-width: 700px)").matches) {
+        let div = document.getElementById("mobile-size-filter");
+        let openIcon = document.getElementById("fa-size-open");
+        let closeIcon = document.getElementById('fa-size-close');
 
-    if (div.style.display == "flex") {
-        // Se estiver aberto
-        div.style.display = "none"; // Fechar
-        openIcon.style.display = "block";
-        closeIcon.style.display = "none";
-    } else {
-        // Se estiver fechado
-        div.style.display = "flex"; // Abrir
-        closeIcon.style.display = "block";
-        openIcon.style.display = "none";
+        if (div.style.display == "flex") {
+            // Se estiver aberto
+            div.style.display = "none"; // Fechar
+            openIcon.style.display = "block";
+            closeIcon.style.display = "none";
+        } else {
+            // Se estiver fechado
+            div.style.display = "flex"; // Abrir
+            closeIcon.style.display = "block";
+            openIcon.style.display = "none";
+        }
     }
 }
 
 // Abrir ou fechar a filtro de Preços (versão mobile)
 function openPriceFilter() {
-    let div = document.getElementById("mobile-price-filter");
-    let openIcon = document.getElementById("fa-price-open");
-    let closeIcon = document.getElementById('fa-price-close');
+    if (window.matchMedia("(max-width: 700px)").matches) {
+        let div = document.getElementById("mobile-price-filter");
+        let openIcon = document.getElementById("fa-price-open");
+        let closeIcon = document.getElementById('fa-price-close');
 
-    if (div.style.display == "flex") {
-        // Se estiver aberto
-        div.style.display = "none"; // Fechar
-        openIcon.style.display = "block";
-        closeIcon.style.display = "none";
-    } else {
-        // Se estiver fechado
-        div.style.display = "flex"; // Abrir
-        closeIcon.style.display = "block";
-        openIcon.style.display = "none";
+        if (div.style.display == "flex") {
+            // Se estiver aberto
+            div.style.display = "none"; // Fechar
+            openIcon.style.display = "block";
+            closeIcon.style.display = "none";
+        } else {
+            // Se estiver fechado
+            div.style.display = "flex"; // Abrir
+            closeIcon.style.display = "block";
+            openIcon.style.display = "none";
+        }
     }
 }
 
